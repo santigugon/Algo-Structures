e19fd854983fdedd6f56e1d0757a43ed72f18a3d
santigugon <santigugon@gmail.com>
2023-10-03 09:41:06 -0600

    Altura corregida tarjetas

--- src/creatorCard.tsx
+++ src/creatorCard.tsx
@@ -45,6 +45,58 @@ export function CreatorCard({
           </a>
         </div>
       </div>
+      <div className="nes-container">
+        <div className="creator-box">
+          <img src={imgSrc} alt={`creator image ${name}`} />
+        </div>
+        <h2>{name}</h2>
+        {description}
+
+        {technologies.map((tech) => {
+          return <li>{tech}</li>;
+        })}
+
+        <div className="creator-icons">
+          <a href={github}>
+            <i className="nes-icon github is-medium"></i>
+          </a>
+          <a
+            href={`mailto:${email}?subject=Algo-Structures
+          &body=Hi%20${name},%20I'm%20contacting%20you%20because...`}
+          >
+            <i className="nes-icon gmail is-medium"></i>
+          </a>
+          <a href={linkedin}>
+            <i className="nes-icon linkedin is-medium"></i>
+          </a>
+        </div>
+      </div>
+      <div className="nes-container">
+        <div className="creator-box">
+          <img src={imgSrc} alt={`creator image ${name}`} />
+        </div>
+        <h2>{name}</h2>
+        {description}
+
+        {technologies.map((tech) => {
+          return <li>{tech}</li>;
+        })}
+
+        <div className="creator-icons">
+          <a href={github}>
+            <i className="nes-icon github is-medium"></i>
+          </a>
+          <a
+            href={`mailto:${email}?subject=Algo-Structures
+          &body=Hi%20${name},%20I'm%20contacting%20you%20because...`}
+          >
+            <i className="nes-icon gmail is-medium"></i>
+          </a>
+          <a href={linkedin}>
+            <i className="nes-icon linkedin is-medium"></i>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
