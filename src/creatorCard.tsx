6b363ba703b773e111a27a7c5a19d67408151725
santigugon <santigugon@gmail.com>
2023-10-03 18:07:01 -0600

    Nav menu finalizado

--- src/creatorCard.tsx
+++ src/creatorCard.tsx
@@ -1,4 +1,4 @@
-interface CreatorCardProps {
+interface Creator {
   name: string;
   description: string;
   imgSrc: string;
@@ -16,87 +16,57 @@ export function CreatorCard({
   linkedin,
   email,
   technologies,
-}: CreatorCardProps) {
+}: Creator) {
+  return (
+    <div className="nes-container">
+      <div className="creator-box">
+        <img src={imgSrc} alt={`creator image ${name}`} />
+      </div>
+      <h2>{name}</h2>
+      {description}
+
+      {technologies.map((tech) => {
+        return <li>{tech}</li>;
+      })}
+
+      <div className="creator-icons">
+        <a href={github}>
+          <i className="nes-icon github is-medium"></i>
+        </a>
+        <a
+          href={`mailto:${email}?subject=Algo-Structures
+          &body=Hi%20${name},%20I'm%20contacting%20you%20because...`}
+        >
+          <i className="nes-icon gmail is-medium"></i>
+        </a>
+        <a href={linkedin}>
+          <i className="nes-icon linkedin is-medium"></i>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+interface CreatorCardsProps {
+  creators: Creator[];
+}
+
+export function CreatorCards({ creators }: CreatorCardsProps) {
   return (
     <div className="cards-container">
-      <div className="nes-container">
-        <div className="creator-box">
-          <img src={imgSrc} alt={`creator image ${name}`} />
-        </div>
-        <h2>{name}</h2>
-        {description}
-
-        {technologies.map((tech) => {
-          return <li>{tech}</li>;
-        })}
-
-        <div className="creator-icons">
-          <a href={github}>
-            <i className="nes-icon github is-medium"></i>
-          </a>
-          <a
-            href={`mailto:${email}?subject=Algo-Structures
-          &body=Hi%20${name},%20I'm%20contacting%20you%20because...`}
-          >
-            <i className="nes-icon gmail is-medium"></i>
-          </a>
-          <a href={linkedin}>
-            <i className="nes-icon linkedin is-medium"></i>
-          </a>
-        </div>
-      </div>
-      <div className="nes-container">
-        <div className="creator-box">
-          <img src={imgSrc} alt={`creator image ${name}`} />
-        </div>
-        <h2>{name}</h2>
-        {description}
-
-        {technologies.map((tech) => {
-          return <li>{tech}</li>;
-        })}
-
-        <div className="creator-icons">
-          <a href={github}>
-            <i className="nes-icon github is-medium"></i>
-          </a>
-          <a
-            href={`mailto:${email}?subject=Algo-Structures
-          &body=Hi%20${name},%20I'm%20contacting%20you%20because...`}
-          >
-            <i className="nes-icon gmail is-medium"></i>
-          </a>
-          <a href={linkedin}>
-            <i className="nes-icon linkedin is-medium"></i>
-          </a>
-        </div>
-      </div>
-      <div className="nes-container">
-        <div className="creator-box">
-          <img src={imgSrc} alt={`creator image ${name}`} />
-        </div>
-        <h2>{name}</h2>
-        {description}
-
-        {technologies.map((tech) => {
-          return <li>{tech}</li>;
-        })}
-
-        <div className="creator-icons">
-          <a href={github}>
-            <i className="nes-icon github is-medium"></i>
-          </a>
-          <a
-            href={`mailto:${email}?subject=Algo-Structures
-          &body=Hi%20${name},%20I'm%20contacting%20you%20because...`}
-          >
-            <i className="nes-icon gmail is-medium"></i>
-          </a>
-          <a href={linkedin}>
-            <i className="nes-icon linkedin is-medium"></i>
-          </a>
-        </div>
-      </div>
+      {creators.map((creator) => {
+        return (
+          <CreatorCard
+            name={creator.name}
+            description={creator.description}
+            imgSrc={creator.imgSrc}
+            github={creator.github}
+            linkedin={creator.linkedin}
+            email={creator.email}
+            technologies={creator.technologies}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/home/Intro.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+
+export const Intro: React.FC = () => {
+  useEffect(() => {
+    const background = document.querySelector(
+      ".background-image"
+    ) as HTMLDivElement;
+
+    const handleScroll = () => {
+      const scrollValue = window.scrollY;
+      background.style.transform = `translateY(${scrollValue * 0.5}px)`; // Adjust the speed of the parallax effect
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      // Clean up the event listener when the component unmounts
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="intro-container">
+      <div className="background-image"></div>
+      <div className="intro-text">
+        <h1>ALGOSTRUCTURES</h1>
+        <br />
+        <h2>Algorithms & Data Structures</h2>
+      </div>
+    </div>
+  );
+};
